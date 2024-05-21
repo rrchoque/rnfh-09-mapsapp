d3fe4db9d234f89a5ccc2833888a05fdf1b9cf33
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-21 06:55:20 -0400

    Se instaló y configuró react-native-maps

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native'
 import { StackNavigator } from './presentation/navigation/StackNavigator';
 import { PermissionsChecker } from './presentation/providers/PermissionsChecker';
 
+// import {enableLatestRenderer} from 'react-native-maps';
+// enableLatestRenderer();
+
 export const MapsApp = () => {
   return (
     <NavigationContainer>
