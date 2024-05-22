9d1907f0a354f219bcf5dbdde8d28eee8141244e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-22 08:13:16 -0400

    Se instaló react-native-geolocation

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -1,16 +1,23 @@
 import { Platform } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Location } from "../../../infrastructure/interfaces/location";
 
-export const Map = () => {
+interface Props {
+  showsUserLocation?: boolean;
+  initialLocation: Location;
+}
+
+export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
 
   return (
     <>
       <MapView
+        showsUserLocation={showsUserLocation}
         provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE}  // remove if not using Google Maps
         style={{ flex: 1 }}
         region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialLocation.latitude,
+          longitude: initialLocation.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
