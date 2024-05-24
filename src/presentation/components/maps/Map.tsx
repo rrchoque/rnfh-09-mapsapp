1360afea1d33347c23aeec9b46962837a9e2f83f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-24 16:32:04 -0400

    Se agregó la funcionalidad para mover la camara a la ubicación actual

--- src/presentation/components/maps/Map.tsx
+++ src/presentation/components/maps/Map.tsx
@@ -2,6 +2,8 @@ import { Platform } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { Location } from "../../../infrastructure/interfaces/location";
 import { FAB } from "../ui/FAB";
+import { useRef } from "react";
+import { useLocationStore } from "../../store/location/useLocationStore";
 
 interface Props {
   showsUserLocation?: boolean;
@@ -10,20 +12,39 @@ interface Props {
 
 export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
 
+  const mapRef = useRef<MapView>();
+  const cameraLocation = useRef<Location>(initialLocation);
+
+  const { getLocation } = useLocationStore(); 
+
+  const moveCameraToLocation = (location: Location) => {
+    if (!mapRef.current) return;
+    mapRef.current.animateCamera({center: location});
+  };
+
+  const moveToCurrentLocation = async () => {
+    const location = await getLocation();
+    if (!location) return;
+    moveCameraToLocation(location);
+
+    console.log(location)
+  };
+
   return (
     <>
       <MapView
+        ref={map => (mapRef.current = map!)}
         showsUserLocation={showsUserLocation}
         provider={Platform.OS === 'ios' ? undefined : PROVIDER_GOOGLE}  // remove if not using Google Maps
         style={{ flex: 1 }}
         region={{
-          latitude: initialLocation.latitude,
-          longitude: initialLocation.longitude,
+          latitude: cameraLocation.current.latitude,
+          longitude: cameraLocation.current.longitude,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}
       >
-        <Marker 
+        {/* <Marker 
           coordinate={{
             latitude: 37.78825,
             longitude: -122.4324,
@@ -31,12 +52,12 @@ export const Map = ({showsUserLocation = true, initialLocation}: Props) => {
           title="Este es el título del marcador"
           description="Este es el cuerpo del marcador"
           image={require('../../../assets/marker.png')}
-        />
+        /> */}
       </MapView>
 
       <FAB
         iconName="compass-outline"
-        onPress={ () => console.log('hola') }
+        onPress={ moveToCurrentLocation }
         style={{
           bottom: 20,
           right: 20,
